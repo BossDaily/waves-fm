fbb1f018c7faab0b57e8e11188403070dd0d57a2
BossDaily <gboys2008@gmail.com>
2025-06-02 21:13:24 -0500

    upgraded to tailwindcss v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],	optimizeDeps: {
+	plugins: [tailwindcss(), sveltekit()],
+	optimizeDeps: {
 		include: ['@firecms/neat', 'lastfm-ts-api', 'simplex-noise', 'colorthief']
 	}
 });
