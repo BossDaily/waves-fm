1fd111dc99f38bed0206472ce2affde573147132
BossDaily <gboys2008@gmail.com>
2025-06-04 20:23:29 -0500

    form is working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,7 @@
 </svelte:head>
 
 <div class={cn(
-	"grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] absolute inset-0 -z-10 h-full w-full"
+	"grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]"
 )}>
 	<main class="flex flex-col gap-8 row-start-2 items-start sm:items-center">
 		<WavyBackground
