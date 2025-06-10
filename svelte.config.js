c35a0ad6f317f925621bdf31619897fc2372b65d
BossDaily <gboys2008@gmail.com>
2025-06-10 15:40:07 -0500

    svelte docs support

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', '.md'],
+
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import icons from 'unplugin-icons/vite';
+import kitDocs from '@svelteness/kit-docs/node';
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [icons({ compiler: 'svelte' }), kitDocs(), tailwindcss(), sveltekit()],
 	optimizeDeps: {
 		include: ['@firecms/neat', 'lastfm-ts-api', 'simplex-noise', 'colorthief']
 	}
